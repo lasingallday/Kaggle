779efdf8a9965b2e86718416fab53308bba31a24
Jeff Thomas <Jeffrey.Thomas@taxware.com>
2016-06-13 16:11:12 -0400

    Added Visualization Method

--- prudential_10.py
+++ prudential_10.py
@@ -1,16 +1,30 @@
+import subprocess
 import pandas as pd
 import numpy as np
 import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 from numpy import float32
 #from sklearn.feature_extraction.text import CountVectorizer
 
-train = pd.read_csv("/home/jeff/Documents/workspace/Prudential_LI/train.csv")
-clf = DecisionTreeClassifier()	
+def visualize_tree(tree, feature_names):
+    """Create tree png using graphviz.
+    """
+    with open("dt.dot", 'w') as f:
+        export_graphviz(tree, out_file=f,
+                        feature_names=feature_names)
+
+    command = ["dot", "-Tsvg", "dt.dot", "-o", "dt.png"]
+    try:
+        subprocess.check_call(command)
+    except:
+        exit("Could not run dot, ie graphviz, to "
+             "produce visualization")
+
+train = pd.read_csv("C:/Users/jeffrey.thomas/workspace/Prudential_LI/train.csv")
+dt = DecisionTreeClassifier()
 
 #mk_cols = train.loc[:, ["Medical_Keyword_"+str(i) for i in range(1,49)]]
-#mk_list = list(mk_cols)
 mk_cols = train.iloc[:,79:127]
 mk_sum = mk_cols.sum(axis=0)
 mk_cols = mk_cols.append(mk_cols, ignore_index=True)
@@ -23,15 +37,15 @@ print(train)
 
 X = train.drop(['Response'], axis=1)
 y = train['Response']
-print(X.shape)
-print(y.shape)
-#Indicate if NaN's part of dataframe
+X_list = list(X.columns[:])
+#print(y)
+#Indicate if NaN's part of features dataframe
 #print(np.any(np.isnan(X)))
 X = X.fillna(1e6).astype(np.float32)
 y = y.fillna(1e6).astype(np.float32)
 #Replace only columns with NaN's
 #bad_flats = X.select_dtypes(include=['float64'])
 #X = X.replace(bad_flats.astype('float32'))
-print(X.dtypes)
-clf.fit(X,y)
-#clf.predict_proba(train[1,:])
+#print(X.dtypes)
+dt.fit(X,y)
+visualize_tree(dt, X_list)
